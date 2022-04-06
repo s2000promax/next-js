e27b017bbaf07b2778a2df337cb4f000087adb4c
Stan <s2000ProMax@gmail.com>
2022-04-06 17:52:30 +0300

    add scss

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Heading from '../components/Heading';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Home = () => (
   <div className={styles.wrapper}>
